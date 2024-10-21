ed5a09a8c908f41a395e8b551f055c703fa90f46
Calin <ryh9711@autuni.ac.nz>
2024-10-21 17:56:22 +1300

    Removed Duplicate Scripts
    There was duplicate scripts that were causing conflicts

--- Assets/Scripts/MultiplayerInputManager.cs
+++ Assets/Scripts/MultiplayerInputManager.cs
@@ -5,55 +5,55 @@ using UnityEngine.InputSystem;
 
 public class MultiplayerInputManager : MonoBehaviour
 {
-    //Player List
-    public List<IndividualPlayerControls> players = new List<IndividualPlayerControls>();
-    int maxPlayers = 4;
+    ////Player List
+    //public List<IndividualPlayerControls> players = new List<IndividualPlayerControls>();
+    //int maxPlayers = 4;
 
-    public PlayerInputActions inputActions;
+    //public PlayerInputActions inputActions;
 
-    //Delegate can be used for scripts relating to checking/waiting for player to join
-    public delegate void OnPlayerJoined(int playerID);
-    public OnPlayerJoined onPlayerJoined;
+    ////Delegate can be used for scripts relating to checking/waiting for player to join
+    //public delegate void OnPlayerJoined(int playerID);
+    //public OnPlayerJoined onPlayerJoined;
 
-    private void Awake()
-    {
-        InitializeInputs();
+    //private void Awake()
+    //{
+    //    InitializeInputs();
 
 
-    }
+    //}
 
-    //Input setup for detecting different players
-    void InitializeInputs()
-    {
-        inputActions = new PlayerInputActions();
-        inputActions.Player.JoinButton.performed += JoinButtonPerformed;
-        inputActions.Enable();
-    }
+    ////Input setup for detecting different players
+    //void InitializeInputs()
+    //{
+    //    inputActions = new PlayerInputActions();
+    //    inputActions.Player.JoinButton.performed += JoinButtonPerformed;
+    //    inputActions.Enable();
+    //}
 
-    private void JoinButtonPerformed(InputAction.CallbackContext obj)
-    {
-        if (players.Count >= maxPlayers)
-        {
-            return;
-        }
+    //private void JoinButtonPerformed(InputAction.CallbackContext obj)
+    //{
+    //    if (players.Count >= maxPlayers)
+    //    {
+    //        return;
+    //    }
 
-        //Check if the device is already assigned
-        foreach (IndividualPlayerControls player in players)
-        {
-            if (player.inputDevice == obj.control.device)
-            {
-                //Device is already assigned
-                return;
-            }
-        }
+    //    //Check if the device is already assigned
+    //    foreach (IndividualPlayerControls player in players)
+    //    {
+    //        if (player.inputDevice == obj.control.device)
+    //        {
+    //            //Device is already assigned
+    //            return;
+    //        }
+    //    }
 
-        IndividualPlayerControls newPlayer = new IndividualPlayerControls();
-        newPlayer.SetupPlayer(obj, players.Count);
-        players.Add(newPlayer);
+    //    IndividualPlayerControls newPlayer = new IndividualPlayerControls();
+    //    newPlayer.SetupPlayer(obj, players.Count);
+    //    players.Add(newPlayer);
 
-        if(onPlayerJoined != null)
-        {
-            onPlayerJoined.Invoke(newPlayer.playerID);
-        }
-    }
+    //    if(onPlayerJoined != null)
+    //    {
+    //        onPlayerJoined.Invoke(newPlayer.playerID);
+    //    }
+    //}
 }
